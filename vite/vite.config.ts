264a611cfc3c31a3841990008a4b1efb5f8d698f
StormVRguy <spirit.info300@gmail.com>
2025-08-28 18:34:09 +0200

    Online!

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	test: {
-		environment: 'jsdom'
-	},
+	base: '/Sacred-Game-of-Life/', // Replace with your repository name
 	plugins: [react()]
 });
